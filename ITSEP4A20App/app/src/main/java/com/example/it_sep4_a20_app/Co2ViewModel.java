d792e7a930e3a8429ec97af120cd240252f3b824
TheSweetMarmotte <claire.zubiaurre@gmail.com>
2020-11-11 08:50:57 +0100

    Add a drawer

---
+++ ITSEP4A20App/app/src/main/java/com/example/it_sep4_a20_app/Co2ViewModel.java
@@ -0,0 +1,7 @@
+package com.example.it_sep4_a20_app;
+
+import androidx.lifecycle.ViewModel;
+
+public class Co2ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- ITSEP4A20App/app/src/main/java/com/example/it_sep4_a20_app/MainActivity.java
+++ ITSEP4A20App/app/src/main/java/com/example/it_sep4_a20_app/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.it_sep4_a20_app;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity
 {
     //hey
+
     private TextView co2View;
     private MainActivityViewModel viewModel;
+    private DrawerLayout drawerLayout;
+    private NavigationView drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,5 +42,68 @@ public class MainActivity extends AppCompatActivity
                 co2View.setText(co2String);
             }
         });
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.layout_drawer_activity);
+
+        drawer = (NavigationView) findViewById(R.id.view);
+        setupDrawerContent(drawer);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.menu_item_home:
+                fragmentClass = HomeFragment.class;
+                break;
+            case R.id.menu_item_parameter:
+                fragmentClass = ParameterFragment.class;
+                break;
+            case R.id.menu_item_co2:
+                fragmentClass = Co2Fragment.class;
+                break;
+            default:
+                fragmentClass = HomeFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.menu_item_home, fragment).commit();
+
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawerLayout.closeDrawers();
+    }
+
+
+
 }
