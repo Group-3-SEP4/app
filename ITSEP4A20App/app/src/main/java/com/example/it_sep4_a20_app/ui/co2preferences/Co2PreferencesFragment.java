487708729e5036c6ce9a1727fbfa81392b84383a
Tobias Sønderbo <tobias.l.sonderbo@gmail.com>
2020-11-18 10:22:25 +0100

    Add Settings and co2 preference fragment

---
+++ ITSEP4A20App/app/src/main/java/com/example/it_sep4_a20_app/ui/co2preferences/Co2PreferencesFragment.java
@@ -0,0 +1,5 @@
+package com.example.it_sep4_a20_app.ui.co2preferences;
+
+public class Co2PreferencesFragment
+{
+}
---
+++ ITSEP4A20App/app/src/main/java/com/example/it_sep4_a20_app/ui/settings/SettingsFragment.java
@@ -0,0 +1,5 @@
+package com.example.it_sep4_a20_app.ui.settings;
+
+public class SettingsFragment
+{
+}
