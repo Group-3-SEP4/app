1275e8451009d603798666bd6a57c4520a135c7a
Tobias Sønderbo <tobias.l.sonderbo@gmail.com>
2020-12-10 12:42:11 +0100

    Add humidity preference

--- ITSEP4A20App/app/src/main/java/com/example/it_sep4_a20_app/ui/preferences/humiditypreference/HumidityPreferenceFragment.java
+++ ITSEP4A20App/app/src/main/java/com/example/it_sep4_a20_app/ui/preferences/humiditypreference/HumidityPreferenceFragment.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -16,15 +17,48 @@ import com.example.it_sep4_a20_app.R;
 
 public class HumidityPreferenceFragment extends PreferenceFragmentCompat
 {
+    private HumidityPreferenceViewModel mViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View root = super.onCreateView(inflater, container, savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HumidityPreferenceViewModel.class);
+        EditTextPreference maxHumidity = findPreference(getString(R.string.key_maxHumidity));
+        EditTextPreference minHumidity = findPreference(getString(R.string.key_minHumidity));
+        Preference resetHumidity = findPreference(getString(R.string.key_resetHumidity));
+        maxHumidity.setSummary(getString(R.string.current_max_humidity, mViewModel.getStoredMaxHumiditySetting()));
+        minHumidity.setSummary(getString(R.string.current_min_humidity, mViewModel.getStoredMinHumiditySetting()));
 
-        EditTextPreference maxHumidity = findPreference(getString(R.string.key_maxTemperature));
-        EditTextPreference minHumidity = findPreference(getString(R.string.key_maxTemperature));
-        Preference resetHumidity = findPreference(getString(R.string.key_resetTemperature));
+        maxHumidity.setOnPreferenceChangeListener((preference, newValue) ->
+        {
+            boolean valid = numberCheck(newValue);
+            if(valid)
+            {
+                mViewModel.storeMaxHumiditySetting(Integer.parseInt(newValue.toString()));
+                maxHumidity.setSummary(getString(R.string.current_max_humidity, Integer.parseInt(newValue.toString())));
+            }
+            return valid;
+        });
+
+        minHumidity.setOnPreferenceChangeListener(((preference, newValue) ->
+        {
+            boolean valid = numberCheck(newValue);
+            if(valid)
+            {
+                mViewModel.storeMinHumiditySetting(Integer.parseInt(newValue.toString()));
+                minHumidity.setSummary(getString(R.string.current_min_humidity, Integer.parseInt(newValue.toString())));
+            }
+            return valid;
+        }));
+
+        resetHumidity.setOnPreferenceClickListener((preference ->
+        {
+            mViewModel.resetHumiditySettings();
+            maxHumidity.setSummary(getString(R.string.current_max_humidity, mViewModel.getStoredMaxHumiditySetting()));
+            minHumidity.setSummary(getString(R.string.current_min_humidity,mViewModel.getStoredMinHumiditySetting()));
+            return true;
+        }));
 
         return root;
     }
